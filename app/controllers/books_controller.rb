a4b225ca4d497f30e8a64e0317e056cf067046e6
Kousalya <Kousalya@sedintechnologies.com>
2025-03-02 22:04:27 +0530

    added dropdown

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:update, :create, :destroy]
+
+    def dropdown
+      @genres = Book.distinct.pluck(:genre)
+    end
+
+    def books_by_genre
+      @books = Book.where(genre: params[:genre])
+      render json: @books
+    end
+
+    def show
+      @book = Book.find(params[:id])
+      render json: @book
+    end
   
     def index
       @books = Book.all
